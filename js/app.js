933609785227503e856e1c29b8c3400041c63955
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-04-05 17:09:08 -0400

    Stamford Eats

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
 console.log('hello');
+
+// const baseURL = 'https://maps.googleapis.com/maps/api/js?'
+// const apiKey = 'AIzaSyDzkcvrMOgJOpe8uo3XelGFbcJV79cngtI'
+// const queryType = 't='
+// const queryURL = baseURL + apiKey + '&' + queryType
+// $.ajax({
+//     url: queryURL 
+    
+//  }).then((movieData) => {
+//     console.log(queryURL)
+
+// })
+ 
+$(() => {
+
+// const initMap = () => {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 41.0534, lng: 73.5387},
+//         scrollwheel: false,
+//         zoom: 2
+//     });
+//     setPath(addPath);
+// }
+
+// function setPath(callback) {
+//   $.getJSON('./expOneActivityData.json',
+//     function(data) {
+//       // Some looping contstruct to navigate through my JSON file.
+//       // create the expeditionCoordinates array
+//       callback(expeditionCoordinates);
+//     }
+//   );
+// }; 
+
+// function addPath(expeditionCoordinates) {
+//   var trekLine = new google.maps.Polyline({
+//     path: expeditionCoordinates,
+//     geodisc: true,
+//     stokeColor: '#FF0000',
+//     strokeOpacity: 1.0,
+//     strokeWeight: 2
+//   });
+
+//   trekLine.setMap(map);
+// }
+
+// Start of the DOM
+
+
+});
